19b3d57acc8f4256b6c42cfedb0db1ef27ca38b2
danny-hazelcast <danny@hazelcast.com>
2015-12-14 14:08:34 +0200

    hzCmd

--- src/main/java/tests/CQ.java
+++ src/main/java/tests/CQ.java
@@ -8,7 +8,7 @@ import static global.Utils.sleepSeconds;
 
 public class CQ extends Task {
 
-    public LatencyListener latency = new LatencyListener();
+    public LatencyListener latency = new LatencyListener(this);
     public String mapName="a";
     public int keyDomain = Integer.MAX_VALUE;
     private IMap map;
