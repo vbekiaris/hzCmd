00cdaeccce882dd3c051bcd5de4b08b60e46c24b
danny-hazelcast <danny@hazelcast.com>
2016-01-21 22:30:27 +0200

    hzCmd

--- src/main/java/local/HzJvmFactory.java
+++ src/main/java/local/HzJvmFactory.java
@@ -6,7 +6,7 @@ import global.NodeType;
  * Created by danny on 21/01/2016.
  */
 public class HzJvmFactory implements JvmFactory {
-    
+
     public RemoteJvm createJvm(Box box, NodeType type, String id) {
         return new RemoteHzJvm(box, type, id);
     }
