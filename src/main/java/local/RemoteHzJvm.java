fee5d2ad03d268b204b7e4e1eb92e72189068fd1
danny-hazelcast <danny@hazelcast.com>
2016-01-21 22:28:20 +0200

    hzCmd

--- src/main/java/local/RemoteHzJvm.java
+++ src/main/java/local/RemoteHzJvm.java
@@ -12,8 +12,6 @@ public class RemoteHzJvm extends RemoteJvm {
     public static final String hzPath ="$HOME/"+Installer.REMOTE_HZ_LIB+"/";
 
     private final String xmlConfig;
-    private int pid = 0;
-    private String version;
 
     public RemoteHzJvm(Box box, NodeType type, String id) {
         super(box, type, id);
