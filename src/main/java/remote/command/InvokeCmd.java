7a6890c860364bfc78c6ecd6b7ab902ee09c5079
danny-hazelcast <danny@hazelcast.com>
2016-01-24 23:10:37 +0200

    hzCmd

---
+++ src/main/java/remote/command/InvokeCmd.java
@@ -0,0 +1,15 @@
+package remote.command;
+
+import java.io.Serializable;
+
+/**
+ * Created by danny on 22/01/2016.
+ */
+public class InvokeCmd implements Cmd, Serializable{
+
+    private String taskId;
+
+    public void exicute(){
+
+    }
+}
---
+++ src/main/java/remote/command/LoadCmd.java
@@ -0,0 +1,15 @@
+package remote.command;
+
+import java.io.Serializable;
+
+/**
+ * Created by danny on 22/01/2016.
+ */
+public class LoadCmd implements Cmd, Serializable{
+
+    private String taskId;
+
+    public void exicute(){
+
+    }
+}
