19cdffc3c43bf87b1ae973b5e5f59de025966ca8
danny-hazelcast <danny@hazelcast.com>
2016-01-28 00:26:17 +0200

    hzCmd

--- src/main/java/cmdline/Ssh.java
+++ src/main/java/cmdline/Ssh.java
@@ -1,7 +1,10 @@
 package cmdline;
 
+import com.github.rvesse.airline.annotations.Arguments;
 import com.github.rvesse.airline.annotations.Option;
 import main.HzCmd;
+
+import java.util.List;
 
 @com.github.rvesse.airline.annotations.Command(name = "ssh", description = "ssh instruction from cwd of each member client")
 public class Ssh extends Command
@@ -12,10 +15,17 @@ public class Ssh extends Command
     @Option(name = "-cmd", description = "jvm id / name")
     public String cmd = "ls";
 
+    @Arguments(description = "ssh cmd string")
+    private List<String> args;
+
 
     public void exe(HzCmd hzCmd) {
         try {
-            hzCmd.ssh(jvmId, cmd);
+            StringBuilder cmd = new StringBuilder();
+            for (String s : args)
+                cmd.append(s+" ");
+
+            hzCmd.ssh(jvmId, cmd.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
