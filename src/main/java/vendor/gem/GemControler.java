0fd3771091798fdae84946561cce76950b9edae4
danny-hazelcast <danny@hazelcast.com>
2016-02-08 23:15:30 +0200

    g

--- src/main/java/vendor/gem/GemControler.java
+++ src/main/java/vendor/gem/GemControler.java
@@ -27,14 +27,14 @@ public class GemControler extends Controler {
                 serverCache = new CacheFactory().set("cache-xml-file", "server-cache.xml")
                         .set("mcast-port", "0")
                         .set("start-locator", "11001")
-                        .set("locators", "localhost[11001],10.0.0.193[11002]").create();
+                        .set("locators", "10.0.0.192[11002],10.0.0.193[11001]").create();
 
             } else if ( ID.equals("GemMember2F") ) {
 
                 serverCache = new CacheFactory().set("cache-xml-file", "server-cache.xml")
                         .set("mcast-port", "0")
                         .set("start-locator", "11002")
-                        .set("locators", "10.0.0.192[11001],localhost[11002]").create();
+                        .set("locators", "10.0.0.192[11002],10.0.0.193[11001]").create();
 
                 //serverCache = new CacheFactory().set("cache-xml-file", "server-cache.xml").set("mcast-port", "0").set("locators", "10.0.0.193[13489]").create();
             }
