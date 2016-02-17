0ee9a3413b0f0443365d54c4f83cfd7d1bc80afd
danny-hazelcast <danny@hazelcast.com>
2016-02-17 17:35:38 +0200

    hzCmd

--- src/main/java/cmdline/Chart.java
+++ src/main/java/cmdline/Chart.java
@@ -1,4 +1,4 @@
 package cmdline;
 
-@com.github.rvesse.airline.annotations.Command(name="chart", description = "chart your data")
+@com.github.rvesse.airline.annotations.Command(name="chart", description = "needs gnuplot installed. chart your data")
 public class Chart extends Command { }
