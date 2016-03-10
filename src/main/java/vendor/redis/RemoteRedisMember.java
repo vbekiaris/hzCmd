1740ae8db1eebde1e63970210b00f96356522bb4
danny-hazelcast <danny@hazelcast.com>
2016-03-10 22:04:41 +0200

    hzCmd

--- src/main/java/vendor/redis/RemoteRedisMember.java
+++ src/main/java/vendor/redis/RemoteRedisMember.java
@@ -56,6 +56,8 @@ public class RemoteRedisMember extends RemoteJvm {
         System.out.println( box.ssh("gem install redis") );
 
 
+
+        System.out.println("nohup cd +\" +dir+\";\" + \" redis-server >> out.txt 2>&1 & echo $!");
         String pidStr = box.ssh("nohup cd +" +dir+";" + " redis-server >> out.txt 2>&1 & echo $!");
         pid = Integer.parseInt(pidStr.trim());
 
