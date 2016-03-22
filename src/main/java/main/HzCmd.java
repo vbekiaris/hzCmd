32881bf5d5720f693d170b7f943290fd067e123c
danny-hazelcast <danny@hazelcast.com>
2016-03-22 18:46:33 +0200

    hzCmd

--- src/main/java/main/HzCmd.java
+++ src/main/java/main/HzCmd.java
@@ -366,14 +366,14 @@ public class HzCmd implements Serializable {
     public void invokeBenchMark(String jvmId, int threadCound, String taksId) throws Exception {
         invokeSync(jvmId, 1, "init", taksId);
 
-        invokeSync(jvmId, threadCound, "preBench", taksId);
+        invokeSync(jvmId, 1, "preBench", taksId);
         invokeSync(jvmId, threadCound, "warmup", taksId);
-        invokeSync(jvmId, threadCound, "postBench", taksId);
+        invokeSync(jvmId, 1, "postBench", taksId);
 
 
-        invokeSync(jvmId, threadCound, "preBench", taksId);
+        invokeSync(jvmId, 1, "preBench", taksId);
         invokeSync(jvmId, threadCound, "run", taksId);
-        invokeSync(jvmId, threadCound, "postBench", taksId);
+        invokeSync(jvmId, 1, "postBench", taksId);
         //invokeSync(jvmId, 1, "cleanup", taksId);
     }
 
