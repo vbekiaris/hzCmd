1b363eb7923383cf0ad42b4a1f182886dac0c59c
danny-hazelcast <danny@hazelcast.com>
2016-05-26 11:16:44 +0300

    hzCmd

--- src/main/java/cmdline/util/Check.java
+++ src/main/java/cmdline/util/Check.java
@@ -4,7 +4,7 @@ import cmdline.base.Command;
 import com.github.rvesse.airline.annotations.Option;
 import main.HzCmd;
 
-@com.github.rvesse.airline.annotations.Command(name = "check", description = "check remote jvm for error, returns exit code 1 on error")
+@com.github.rvesse.airline.annotations.Command(name = "check", description = "check remote jvm for error, returns exit code 1 at error")
 public class Check extends Command
 {
     @Option(name = "-id", description = "jvm id / name")
