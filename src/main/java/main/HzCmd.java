9ffe225637294bb3fd9d904791d47175eaadf3ed
danny-hazelcast <danny@hazelcast.com>
2016-06-17 11:08:52 +0300

    hzCmd

--- src/main/java/main/HzCmd.java
+++ src/main/java/main/HzCmd.java
@@ -235,8 +235,6 @@ public class HzCmd implements Serializable {
 
         Map<String, ClusterManager> clustersTemp = clusters;
         clusters=null;
-
-        BenchMarkSettings bm = benchMarkSettings;
         benchMarkSettings=null;
 
         Bash.rm(serFile);
