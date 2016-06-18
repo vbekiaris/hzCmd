5fe0651db568127d46c809a1165630f8595daf01
danny-hazelcast <danny@hazelcast.com>
2016-06-18 15:57:59 +0300

    hzCmd

--- src/main/java/cmdline/util/Restart.java
+++ src/main/java/cmdline/util/Restart.java
@@ -5,21 +5,30 @@ import com.github.rvesse.airline.annotations.Arguments;
 import com.github.rvesse.airline.annotations.Option;
 import main.HzCmd;
 
+import java.util.List;
+
 @com.github.rvesse.airline.annotations.Command(name = "restart", description = "restart a jvm in a cluster using its id")
 public class Restart extends Command
 {
-    @Option(name = "-id", description = "jvm id / name")
-    public String jvmId=".*";
-
     @Option(name = "-v", description = "hazelcast version e.g. 9.7")
     public String version;
 
     @Option(name = "-ee", description = "ee version")
     public boolean ee=false;
 
+    @Arguments(description = "jvm id default .*")
+    public List<String> ids;
+
+
     public void exe(HzCmd hzCmd) {
         try {
-            hzCmd.restart(jvmId, version, ee);
+            if(ids==null){
+                hzCmd.restart(".*", version, ee);
+            }else{
+                for (String id : ids) {
+                    hzCmd.restart(id, version, ee);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
