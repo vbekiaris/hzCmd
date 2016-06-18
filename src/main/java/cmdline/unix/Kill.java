36b2cc20c14fa4e3b390fe25225bbf1d8b5dbce3
danny-hazelcast <danny@hazelcast.com>
2016-06-18 15:50:19 +0300

    hzCmd

--- src/main/java/cmdline/unix/Kill.java
+++ src/main/java/cmdline/unix/Kill.java
@@ -1,18 +1,27 @@
 package cmdline.unix;
 
 import cmdline.base.Command;
+import com.github.rvesse.airline.annotations.Arguments;
 import com.github.rvesse.airline.annotations.Option;
 import main.HzCmd;
+
+import java.util.List;
 
 @com.github.rvesse.airline.annotations.Command(name = "kill", description = "kill -9 jvm in cluster")
 public class Kill extends Command
 {
-    @Option(name = "-id", description = "jvm id default .*")
-    public String jvmId=".*";
+    @Arguments(description = "jvm id default .*")
+    public List<String> ids;
 
     public void exe(HzCmd hzCmd) {
         try {
-            hzCmd.kill(jvmId);
+            if(ids==null){
+                hzCmd.kill(".*");
+            }else{
+                for (String id : ids) {
+                    hzCmd.kill(id);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
