a4552e9de7f30a9736a7435d6d2941b19836a36c
danny-hazelcast <danny@hazelcast.com>
2016-07-13 12:21:55 +0300

    hzCmd

---
+++ src/main/java/global/ReplyMsg.java
@@ -0,0 +1,13 @@
+package global;
+
+/**
+ * Created by danny on 13/07/2016.
+ */
+public class ReplyMsg {
+
+    public String id;
+    public String benchId;
+    public String benchClazz;
+    public boolean error=false;
+    public String msg;
+}
