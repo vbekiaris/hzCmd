79af3a5f626d8880e9493001e6db4ba14545533d
danny-hazelcast <danny@hazelcast.com>
2016-07-14 21:29:18 +0300

    hzCmd

--- src/main/java/local/Installer.java
+++ src/main/java/local/Installer.java
@@ -48,7 +48,7 @@ public abstract class Installer {
 
 
         uploadStuff.add(Bash.find(M2_Repo, "metrics-core-3.1.1.jar"));
-        uploadStuff.add(Bash.find(M2_Repo, "HdrHistogram-2.1.8.jar"));
+        uploadStuff.add(Bash.find(M2_Repo, "HdrHistogram-2.1.9.jar"));
 
         uploadStuff.add(Bash.find(M2_Repo, "log4j-1.2.17.jar"));
         uploadStuff.add(Bash.find(M2_Repo, "slf4j-api-1.7.5.jar"));
