acb33c77125918a3581cecda01c36b57fa13cb34
danny-hazelcast <danny@hazelcast.com>
2016-07-24 16:27:15 +0300

    hzCmd

--- src/main/java/cmdline/util/Jfr.java
+++ src/main/java/cmdline/util/Jfr.java
@@ -1,20 +1,35 @@
 package cmdline.util;
 
 import cmdline.base.Command;
+import com.github.rvesse.airline.annotations.Arguments;
 import com.github.rvesse.airline.annotations.Option;
 import local.properties.HzCmdProperties;
 import main.HzCmd;
 
 import java.io.IOException;
+import java.util.List;
 
-@com.github.rvesse.airline.annotations.Command(name = "jfr", description = "turn on jfr")
+@com.github.rvesse.airline.annotations.Command(name = "jfr", description = "no, optional arguments")
 public class Jfr extends Command
 {
+
+    @Arguments(description = "jfr args")
+    public List<String> args;
+
 
     public void exe(HzCmd hzCmd) {
         try {
             HzCmdProperties properties = new HzCmdProperties();
             properties.writePropertie(HzCmdProperties.JFR, "true");
+
+            if(args!=null){
+                String argsStr = new String();
+                for (String arg : args) {
+                    argsStr +=arg+" ";
+                }
+                properties.writePropertie(HzCmdProperties.JFR_ARGS, argsStr);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
