017123cb6caf7f9125d5e95e69f2c7e37b2f7e4f
danny-hazelcast <danny@hazelcast.com>
2016-10-03 14:41:11 +0300

    hzCmd

--- src/main/java/vendor/coherence/CoherenceControler.java
+++ src/main/java/vendor/coherence/CoherenceControler.java
@@ -1,14 +1,28 @@
 package vendor.coherence;
 
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
+
+import com.tangosol.net.DefaultConfigurableCacheFactory;
+import com.tangosol.net.DefaultCacheServer;
+
+
 import global.NodeType;
 import remote.main.Controler;
 
 public class CoherenceControler extends Controler {
 
-    //private NamedCache cache = CacheFactory.getCache();
+    private NamedCache cache = CacheFactory.getCache();
 
     public CoherenceControler(NodeType type) throws Exception {
         super(type);
+
+        DefaultConfigurableCacheFactory factory;
+        factory = new DefaultConfigurableCacheFactory("my-cache-config.xml");
+
+        DefaultCacheServer dcs = new DefaultCacheServer(factory);
+        dcs.startAndMonitor(5000);
+
     }
 
     public void init(NodeType type) throws Exception {
