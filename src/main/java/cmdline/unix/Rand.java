f3c19fb638ab87ca1e9b3042801302e36a0825e3
danny-hazelcast <danny@hazelcast.com>
2017-01-06 18:35:45 +0700

    hzCmd

--- src/main/java/cmdline/unix/Rand.java
+++ src/main/java/cmdline/unix/Rand.java
@@ -5,22 +5,23 @@ import com.github.rvesse.airline.annotations.Option;
 import main.HzCmd;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static global.Utils.rangeMap;
 
-@com.github.rvesse.airline.annotations.Command(name = "rand", description = "random int between -min inclusive  -max exclusive")
+@com.github.rvesse.airline.annotations.Command(name = "rand", description = "random int between -min inclusive  -max inclusive")
 public class Rand extends Command
 {
     @Option(name = "-min", description = "min default 0")
     public int min=0;
 
-    @Option(name = "-max", description = "max default 100")
-    public int max=100;
+    @Option(name = "-max", description = "max default 1")
+    public int max=1;
 
     public void exe(HzCmd hzCmd) {
 
         Random random = new Random();
 
-        System.out.println( rangeMap(random.nextInt(), min, max) );
+        System.out.println( rangeMap(random.nextInt(), min, max+1) );
     }
 }
